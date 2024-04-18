fc1bea792e97eb14ce2f16aeaf3784b94b5ee428
Anne Tumlin <amtumlin@gmail.com>
2024-04-17 19:39:16 -0500

    Got the code working.

--- Data/Source_Domain/StandardDatasetGeneration.m
+++ Data/Source_Domain/StandardDatasetGeneration.m
@@ -4,13 +4,14 @@ clear
 
 k=0;
 Basecase_index = 1;
-while(1)
+Dataset = [];
 
+while(1)
 [PdQd,p,q,p_attacked,q_attacked,success] = Attack_generation(Basecase_index);
 if success~=1 
     continue;
 else
-    k = k + 1
+    k = k + 1;
     Basecase_index = Basecase_index+1;
     if Basecase_index>100
         Basecase_index = 1;
@@ -18,7 +19,16 @@ else
     Dataset([2*k-1,2*k],:) = [PdQd',p',q',0;    
                     PdQd',p_attacked',q_attacked',1];             
 end
-if k>=500000
+if k>=5
     break;
 end
 end
+
+% Save the dataset to a file
+save('IEEE_13Bus_Dataset.mat', 'Dataset');
+
+% View the size of the Dataset
+size(Dataset)
+
+% View the first few rows of the Dataset
+head(Dataset)
